d9370127a298017698b6e1960e3543e6b3ccecc8
dimov95 <95aleksandyr@gmail.com>
2025-04-05 22:39:40 +0300

    Git Configures actions workflow.

--- SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
+++ SeleniumIDE/obj/Debug/net6.0/SeleniumIde.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870858bcc8543100ca5f1c2dfe798b3d98491d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeleniumIde")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
